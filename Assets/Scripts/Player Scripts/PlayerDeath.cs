195dddcae68966421375a7e233344ce96770b6b5
anirml <jeppelj@yahoo.dk>
2021-06-12 13:41:54 +0200

    player death

--- Assets/Scripts/Player Scripts/PlayerDeath.cs
+++ Assets/Scripts/Player Scripts/PlayerDeath.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class PlayerDeath : MonoBehaviourPun
 {
@@ -19,9 +20,29 @@ public class PlayerDeath : MonoBehaviourPun
             // TODO: FIX FOR MULTIPLAYER
             //PhotonNetwork.Destroy(this.gameObject);
             //PhotonNetwork.JoinRoom("QuickStartMenuDemo");
-            Object.Destroy(this.gameObject);
+            //PhotonNetwork.JoinRoom("QuickStartMenuDemo");
+            PhotonNetwork.Destroy(this.gameObject);
+            //PhotonNetwork.Disconnect();
+            //PhotonNetwork.Reconnect();
+            //PhotonNetwork.Destroy(this.gameObject);
+            //PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.LeaveRoom(this.gameObject);
+            Debug.Log("er vi her?");
+            PhotonNetwork.Disconnect();
+            //RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)20 };
+            //PhotonNetwork.JoinOrCreateRoom("QuickStartMenuDemo",roomOps, null);		
+            
         }
 
+    }
+
+
+    void OnDisconnected(){
+        Debug.Log("Player Left Rooom");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    void OnLeftRoom(){
+        Debug.Log("Player Left Rooom");
     }
 
     void OnDestroy()
