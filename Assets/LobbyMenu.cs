57ee560d844ca7f123d69abc21dbf6d85e72da0b
malthew <49302195+malthew@users.noreply.github.com>
2021-06-13 16:13:03 +0200

    Added more buttons to lobby and photon to randomize player color

---
+++ Assets/LobbyMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyMenu : MonoBehaviour
+{
+public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
