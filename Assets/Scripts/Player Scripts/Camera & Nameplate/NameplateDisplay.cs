f5cc2c1329c45280f7798138b1922478b4cb7d38
anirml <jeppelj@yahoo.dk>
2021-06-16 12:36:09 +0200

    Nameplate Display fixed

--- Assets/Scripts/Player Scripts/Camera & Nameplate/NameplateDisplay.cs
+++ Assets/Scripts/Player Scripts/Camera & Nameplate/NameplateDisplay.cs
@@ -6,11 +6,21 @@ public class NameplateDisplay : MonoBehaviourPun
 {
     [SerializeField] private TextMeshProUGUI nameText;
 
-    private void Start() 
+    Quaternion rotation;
+    private void Start()
     {
         SetName();
     }
 
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = rotation;
+    }
     private void SetName()
     {
         nameText.text = photonView.Owner.NickName;
