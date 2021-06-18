4b33481718e1e98cd04a5ca288c67b12fab4578a
MartinBrandstrup <44898614+MartinBrandstrup@users.noreply.github.com>
2021-06-18 15:29:02 +0200

    Update PlayerEvents.cs

--- Assets/Scripts/Player Scripts/PlayerEvents.cs
+++ Assets/Scripts/Player Scripts/PlayerEvents.cs
@@ -52,7 +52,6 @@ public class PlayerEvents : MonoBehaviourPun
     public void OnPlayerDeath(int ownId)
     {
         PlayerDeath?.Invoke(ownId);
-        PlayerRespawn?.Invoke(ownId);
     }
 
 }
